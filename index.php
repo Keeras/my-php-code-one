43416c7f7fc2323c5bccea738d5d5f6c70742869
Keeras <36989417+Keeras@users.noreply.github.com>
2018-03-05 17:16:23 +0545

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>My First Project</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+</head>
+<body>
+	<h1>Welcome to my project !</h1>
+
+	<h3>Courses</h3>
+	<ul>
+		<li>Basic PHP</li>
+		<li>Advance</li>
+		<li>PRO</li>
+	</ul>
+</body>
+</html>
